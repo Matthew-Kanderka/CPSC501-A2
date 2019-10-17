fdb56cc64258af9b22e3d3b203b7510fdaba8891
Matthew Kanderka <kanderkam@hotmail.com>
2019-10-16 20:01:26 -0600

    Name declaring class, superclass and interface

--- Inspector.java
+++ Inspector.java
@@ -1,4 +1,3 @@
-
 public class Inspector {
 
     public void inspect(Object obj, boolean recursive) {
@@ -7,6 +6,39 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+
+        System.out.println("Class Name: " + c.getName());
+
+        getSuperClassInfo(c, obj, recursive, depth);
+        getInterfaceInfo(c, obj, recursive, depth);
+    }
+
+    private void getSuperClassInfo(Class c, Object obj, boolean recursive, int depth) {
+
+        if (c.equals(Object.class)) {
+            return;
+        }
+
+        Class superClass = c.getSuperclass();
+
+        if (superClass != null) {
+            System.out.println("Super Class Name: " + superClass.getName());
+
+            inspectClass(superClass, obj, recursive, depth + 1);
+        } else {
+            System.out.println("No super class");
+        }
+    }
+
+    private void getInterfaceInfo(Class c, Object obj, boolean recursive, int depth) {
+
+        Class[] interfaces = c.getInterfaces();
+
+        for (Class interf: interfaces) {
+
+            System.out.println("Implements Interface: " + interf.getName());
+            inspectClass(interf, obj, recursive, depth + 1);
+        }
     }
 
 }
