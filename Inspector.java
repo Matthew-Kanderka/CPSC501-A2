1e13c56f05b7bf67ec61f049f03d0149c786724c
Matthew Kanderka <kanderkam@hotmail.com>
2019-10-16 20:20:54 -0600

    List info about constructors and methods the class declares

--- Inspector.java
+++ Inspector.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Inspector {
 
     public void inspect(Object obj, boolean recursive) {
@@ -11,6 +14,9 @@ public class Inspector {
 
         getSuperClassInfo(c, obj, recursive, depth);
         getInterfaceInfo(c, obj, recursive, depth);
+        getConstructorInfo(c, obj, depth);
+        getMethodInfo(c, obj, depth);
+        getFieldInfo(c, obj, depth);
     }
 
     private void getSuperClassInfo(Class c, Object obj, boolean recursive, int depth) {
@@ -41,4 +47,49 @@ public class Inspector {
         }
     }
 
+    private void getConstructorInfo(Class c, Object obj, int depth) {
+
+        Constructor[] constructors = c.getConstructors();
+
+        for (Constructor constructor: constructors) {
+            System.out.println("Constructor Name: " + constructor.getName());
+
+            System.out.println("Constructor Parameter Types: ");
+            Class[] parameterTypes = constructor.getParameterTypes();
+            for (Class pt: parameterTypes) {
+                System.out.print(pt.getName());
+            }
+
+            System.out.println("Constructor Modifiers: " + constructor.getModifiers());
+        }
+    }
+
+    private void getMethodInfo(Class c, Object obj, int depth) {
+
+        Method[] methods = c.getMethods();
+
+        for (Method m: methods) {
+            System.out.println("Method Name: " + m.getName());
+
+            System.out.println("Exceptions Thrown:");
+            Class[] exceptions = m.getExceptionTypes();
+            for (Class ex: exceptions) {
+                System.out.print(ex.getName());
+            }
+
+            System.out.println("Parameter Types:");
+            Class[] parameterTypes = m.getParameterTypes();
+            for (Class pt: parameterTypes) {
+                System.out.print(pt.getName());
+            }
+
+            System.out.println("Return Type: " + m.getReturnType().getName());
+
+            System.out.println("Modifiers: " + m.getModifiers());
+        }
+    }
+
+    private void getFieldInfo(Class c, Object obj, int depth) {
+
+    }
 }
