c8849b4f685209d1b14da296ff87f09df7d32bec
Matthew Kanderka <kanderkam@hotmail.com>
2019-10-18 17:09:45 -0600

    Formatted output with tabs and completed get field info and array inspection

--- Inspector.java
+++ Inspector.java
@@ -1,5 +1,4 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public class Inspector {
 
@@ -10,13 +9,13 @@ public class Inspector {
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
 
-        System.out.println("Class Name: " + c.getName());
+        formattedPrint("Class Name: " + c.getName(), depth);
 
         getSuperClassInfo(c, obj, recursive, depth);
         getInterfaceInfo(c, obj, recursive, depth);
         getConstructorInfo(c, obj, depth);
         getMethodInfo(c, obj, depth);
-        getFieldInfo(c, obj, depth);
+        getFieldInfo(c, obj, recursive, depth);
     }
 
     private void getSuperClassInfo(Class c, Object obj, boolean recursive, int depth) {
@@ -28,11 +27,10 @@ public class Inspector {
         Class superClass = c.getSuperclass();
 
         if (superClass != null) {
-            System.out.println("Super Class Name: " + superClass.getName());
-
+            formattedPrint("Super Class Name: " + superClass.getName(), depth);
             inspectClass(superClass, obj, recursive, depth + 1);
         } else {
-            System.out.println("No super class");
+            formattedPrint("No super class", depth);
         }
     }
 
@@ -41,55 +39,110 @@ public class Inspector {
         Class[] interfaces = c.getInterfaces();
 
         for (Class interf: interfaces) {
-
-            System.out.println("Implements Interface: " + interf.getName());
+            formattedPrint("Implements Interface: " + interf.getName(), depth);
             inspectClass(interf, obj, recursive, depth + 1);
         }
     }
 
     private void getConstructorInfo(Class c, Object obj, int depth) {
 
-        Constructor[] constructors = c.getConstructors();
+        Constructor[] constructors = c.getDeclaredConstructors();
 
         for (Constructor constructor: constructors) {
-            System.out.println("Constructor Name: " + constructor.getName());
+            formattedPrint("Constructor Name: " + constructor.getName(), depth);
 
-            System.out.println("Constructor Parameter Types: ");
             Class[] parameterTypes = constructor.getParameterTypes();
             for (Class pt: parameterTypes) {
-                System.out.print(pt.getName());
+                formattedPrint("Parameter Types: " + pt.getName(), depth+1);
             }
 
-            System.out.println("Constructor Modifiers: " + constructor.getModifiers());
+            formattedPrint("Modifiers: " + Modifier.toString(constructor.getModifiers()), depth+1);
         }
     }
 
     private void getMethodInfo(Class c, Object obj, int depth) {
 
-        Method[] methods = c.getMethods();
+        Method[] methods = c.getDeclaredMethods();
 
         for (Method m: methods) {
-            System.out.println("Method Name: " + m.getName());
+            formattedPrint("Method Name: " + m.getName(), depth);
 
-            System.out.println("Exceptions Thrown:");
             Class[] exceptions = m.getExceptionTypes();
             for (Class ex: exceptions) {
-                System.out.print(ex.getName());
+                formattedPrint("Exceptions Thrown: " + ex.getName(), depth+1);
             }
 
-            System.out.println("Parameter Types:");
             Class[] parameterTypes = m.getParameterTypes();
             for (Class pt: parameterTypes) {
-                System.out.print(pt.getName());
+                formattedPrint("Parameter Types: " + pt.getName(), depth+1);
             }
 
-            System.out.println("Return Type: " + m.getReturnType().getName());
-
-            System.out.println("Modifiers: " + m.getModifiers());
+            formattedPrint("Return Type: " + m.getReturnType().getName(), depth+1);
+            formattedPrint("Modifiers: " + Modifier.toString(m.getModifiers()), depth+1);
         }
     }
 
-    private void getFieldInfo(Class c, Object obj, int depth) {
+    private void getFieldInfo(Class c, Object obj, boolean recursive, int depth) {
 
+        Field[] fields = c.getDeclaredFields();
+
+        for (Field f: fields) {
+            formattedPrint("Field Name: " + f.getName(), depth);
+            formattedPrint("Type: " + f.getType().getName(), depth+1);
+            formattedPrint("Modifiers: " + Modifier.toString(f.getModifiers()), depth+1);
+
+            f.setAccessible(true);
+
+            Object value = null;
+            try {
+                value = f.get(obj);
+            } catch (IllegalAccessException e) {
+                System.out.println("Cannot Access Field");
+            }
+
+            if (f.getType().isArray()) {
+                inspectArray(f.getType(), value, recursive, depth);
+            } else if (f.getType().isPrimitive()) {
+                formattedPrint("Value: " + value, depth+1);
+            } else {
+                if (recursive) {
+                    inspectClass(f.getType(), value, recursive, depth + 1);
+                } else {
+                    formattedPrint("Reference Value: " + f.getType().getName() + "@" + f.hashCode(), depth);
+                }
+            }
+        }
+    }
+
+    private void inspectArray(Class c, Object obj, boolean recursive, int depth) {
+
+        Class componentType = c.getComponentType();
+        formattedPrint("Component Type: " + componentType.getName(), depth);
+
+        int arrayLength = Array.getLength(obj);
+        formattedPrint("Array Length: " + arrayLength, depth);
+
+        for (int i = 0; i < arrayLength; i++) {
+            Object object = Array.get(obj, i);
+
+            if (componentType.isPrimitive()) {
+                formattedPrint(object.getClass().getName(), depth);
+            } else if (componentType.isArray()) {
+                inspectArray(object.getClass(), object, recursive, 1);
+            } else {
+                if (recursive) {
+                    inspectClass(object.getClass(), object, recursive, depth + 1);
+                } else {
+                    formattedPrint("Reference Value: " + object.getClass().getName() + "@" + object.hashCode(), depth);
+                }
+            }
+        }
+    }
+
+    private void formattedPrint(String message, int depth) {
+        for (int i = 0; i < depth; i++) {
+            System.out.print("\t");
+        }
+        System.out.println(message);
     }
 }
